b7a6bc5ba01765921aed62febb115f348639cf84
DoraStark <doraschneesturmin@gmail.com>
2024-01-09 19:42:13 +0100

    Added new system out println

--- src/Lesson4Git.java
+++ src/Lesson4Git.java
@@ -2,6 +2,6 @@ public class Lesson4Git {
 
     public static void main(String[] args) {
         System.out.println("Hello world from Daria to Git");
-
+        System.out.println("Second Commit to Git");
     }
 }
